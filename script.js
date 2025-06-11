20ba3c2eaf226e94c92bc685144b64b83abaca6c
Laura Fernanda Garcia <lauragarcia.cv@gmail.com>
2025-06-10 23:03:45 -0300

    Ingreso de promp alert y let

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("Bienvenido a los premios Martin fierro - Actores");
+
+let apellidoactor = (prompt("Ingrese el apellido del actor argentino que más le guste"));
+alert("El apellido es: " + (apellidoactor) );
