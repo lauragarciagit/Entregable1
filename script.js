0fc218036077b4817225db72779ee4c4a32f0756
Laura Fernanda Garcia <lauragarcia.cv@gmail.com>
2025-06-10 23:19:37 -0300

    Se agrego const y console log

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 alert("Bienvenido a los premios Martin fierro - Actores");
 
-let apellidoactor = (prompt("Ingrese el apellido del actor argentino que más le guste"));
-alert("El apellido es: " + (apellidoactor) );
+alert("A continuación les diré los nominados a mejor actor protagónico de novela")
+
+const actores = ["Joaquín Furriel", "Oscar Ferreiro", "Pablo Echarri"];
+console.log(actores);
+
+
+alert("Decime por favor para vos quién ganó de los tres")
+
+let nombreapellido = (prompt("Ingrese el nombre y apellido del actor argentino que más le guste"));
+alert("El apellido es: " + (nombreapellido) );
+
