6393ed3405f81f969db3d5051f6e11973be6d2a1
Laura Fernanda Garcia <lauragarcia.cv@gmail.com>
2025-06-11 01:48:29 -0300

    Ingreso de funciones y let

--- script.js
+++ script.js
@@ -2,12 +2,41 @@ alert("Bienvenido a los premios Martin fierro - Actores");
 
 alert("A continuación les diré los nominados a mejor actor protagónico de novela")
 
-const actores = ["Joaquín Furriel", "Oscar Ferreiro", "Pablo Echarri"];
-console.log(actores);
+console.log("Los nominados son:");
 
 
-alert("Decime por favor para vos quién ganó de los tres")
 
-let nombreapellido = (prompt("Ingrese el nombre y apellido del actor argentino que más le guste"));
-alert("El apellido es: " + (nombreapellido) );
+
+function nominados() {
+    alert("Los nominados son:" + nominados);
+    let ganador = prompt("Quién de los tres piensa que fue el ganador:");
+    adivinaganador(ganador);
+}
+
+function adivinaganador(ganador){
+    if(ganador === "Oscar Ferreiro"){
+        alert("Adivinaste! fue el ganador a mejor actor protagónico");
+    }
+        
+        else{
+            alert("No fue el ganador. Intente nuevamente");
+        }
+
+    }
+
+ 
+ 
+
+nominados("Oscar Ferreiro");
+nominados("Joaquín Furriel");
+nominados("Pablo Echarri");
+
+
+/*   return nominados;
+}
+
+let ganador = preguntarganador;
+
+   
+console.log(ganador);*/
 
