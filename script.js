7f719ec9d0eafd4f36fe6d3ff735589033e9585d
Laura Fernanda Garcia <lauragarcia.cv@gmail.com>
2025-06-12 00:24:05 -0300

    probando switch

--- script.js
+++ script.js
@@ -2,35 +2,42 @@ alert("Bienvenido a los premios Martin fierro - Actores");
 
 alert("A continuación les diré los nominados a mejor actor protagónico de novela")
 
-nominados(["Oscar Ferreiro", "Joaquín Furriel", "Pablo Echarri"]);
+
+
+
 
 
 function nominados(nombre) {
     console.log("Los nominados son: " + nombre.join(" , "));
 }
-
 nominados(["Oscar Ferreiro", "Joaquín Furriel", "Pablo Echarri"]);
 
 
 let respuesta = "";
 
 
-while (respuesta !== "oscar ferreiro") {
+while (respuesta !== "oscar" && respuesta !== "ferreiro") {
     respuesta = prompt("Quién de los tres piensa que fue el ganador");
     
 
 
 
-    if(respuesta === "oscar ferreiro") {
-        alert("Adivinaste! fue el ganador a mejor actor protagónico.");
-    
-    }  else if (respuesta === "oscar") {
+    switch (respuesta) {
+        case "oscar ferreiro":
+            alert("Adivinaste! fue el ganador a mejor actor protagónico.");
+            break;
+        case "oscar":
+            alert("Falta el apellido. Intente nuevamente");
+        case "ferreiro":
+            alert("Falta el nombre. Tiene que ser exactamente igual. Intenta nuevamente");
+
+        default:
+            alert("No fue el ganador. No adivinaste");
+            break;
+  
+        else {
         
-        alert("Falta el apellido. Intente nuevamente");
             
-    }  else {
-        
-        alert("No fue el ganador. Intente nuevamente");
 
     }    
     
