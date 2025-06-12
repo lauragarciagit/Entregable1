040d6ec163789a2b7e3e52fd17e994dffb3b87e4
Laura Fernanda Garcia <lauragarcia.cv@gmail.com>
2025-06-11 22:22:36 -0300

    Correcciones cond

--- script.js
+++ script.js
@@ -16,26 +16,25 @@ nominados(["Oscar Ferreiro", "Joaquín Furriel", "Pablo Echarri"]);
 
 let respuesta = "";
 while (respuesta !== "oscar ferreiro") {
-    respuesta = prompt("Quién de los tres piensa que fue el ganador:");}
+    respuesta = prompt("Quién de los tres piensa que fue el ganador:"); 
 
 
 
     if(respuesta === "oscar ferreiro") {
-        break;
-       
         alert("Adivinaste! fue el ganador a mejor actor protagónico.");
 
-    }  else if (respuesta === "oscar" && respuesta != "ferreiro"); {
+    }  else if (respuesta === "oscar"); {
         
-        alert("No es correcto el apellido. Intente nuevamente");
+        alert("Falta el apellido. Intente nuevamente");
             
-    }  else (respuesta !== "oscar" && respuesta !== "ferreiro"); {
-        alert("No fue el ganador. Intente nuevamente")
-        continue;
+    }  else {
+        
+        alert("No fue el ganador. Intente nuevamente");
+       
 
     }
 
-
+}
 
 
 
