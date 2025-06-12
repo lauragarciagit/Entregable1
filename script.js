f5422885fbcdfb75ecc886754dab07abcf1210c3
Laura Fernanda Garcia <lauragarcia.cv@gmail.com>
2025-06-12 00:51:44 -0300

    opcion condicionales

--- script.js
+++ script.js
@@ -18,48 +18,19 @@ let respuesta = "";
 
 while (respuesta !== "oscar" && respuesta !== "ferreiro") {
     respuesta = prompt("Quién de los tres piensa que fue el ganador");
-    
 
+    if(respuesta === "oscar ferreiro") {
+        break;
+       
+        alert("Adivinaste! fue el ganador a mejor actor protagónico.");
 
-
-    switch (respuesta) {
-        case "oscar ferreiro":
-            alert("Adivinaste! fue el ganador a mejor actor protagónico.");
-            break;
-        case "oscar":
-            alert("Falta el apellido. Intente nuevamente");
-        case "ferreiro":
-            alert("Falta el nombre. Tiene que ser exactamente igual. Intenta nuevamente");
-
-        default:
-            alert("No fue el ganador. No adivinaste");
-            break;
-  
-        else {
+    }  else if (respuesta === "oscar" && respuesta != "ferreiro") {
         
+        alert("No es correcto el apellido. Intente nuevamente");
             
-
+    }  else {
+        alert("No fue el ganador. Intente nuevamente");
     }    
-    
+       
 
 }
-
-
-
-
-
-
-
- 
-
-
-
-
-/*   return nominados;
-}
-
-let ganador = preguntarganador;
-
-   
-console.log(ganador);*/
-
